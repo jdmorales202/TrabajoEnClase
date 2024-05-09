381604789f892d04515a9b3304afebe42494f8c5
jdmorales1234 <juan.morales@surinter.com.co>
2024-05-08 19:57:01 -0500

    Primera Prueba

---
+++ model/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    username:{
+        type: String,
+        required: true
+    },
+    fullname:{
+        type: String,
+        required: true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    rol:{
+        type: String,
+        required: true
+    },
+});
+
+module.exports = mongoose.model('User', userSchema);
